779bc4d9f9928e70be789eabeabfa7a658c9f296
Eduardo Riki <eduardomatushita1@gmail.com>
2023-09-20 06:14:32 -0300

    🎨 Corrigindo rota

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { CADASTRAR_USUARIO, PAGINA_INICIAL, CADASTRAR_CURSO, CADASTRAR_AVALIACAO } from "./routes";
 
 import { PaginaInicial } from "./pages/PaginaInicial";
-import { Cadastro } from "./pages/Cadastro";
+import { CadastrarUsuario } from "./pages/Usuario/Cadastrar";
 import { CadastrarCurso } from "./pages/Curso/Cadastrar";
 import { Avaliacao } from "./pages/Avaliacao/Cadastrar";
 
@@ -11,7 +11,7 @@ export const App = () => {
     <BrowserRouter>
       <Routes>
         <Route exact path={PAGINA_INICIAL} element={<PaginaInicial />} />
-        <Route exact path={CADASTRAR_USUARIO} element={<Cadastro />} />
+        <Route exact path={CADASTRAR_USUARIO} element={<CadastrarUsuario />} />
         <Route exact path={CADASTRAR_CURSO} element={<CadastrarCurso />} />
         <Route exact path={CADASTRAR_AVALIACAO} element={<Avaliacao />} />
       </Routes>

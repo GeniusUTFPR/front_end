b7ec70f056f783eb11aaa58b4f75d50b96e545f0
Eduardo Riki <eduardomatushita1@gmail.com>
2023-09-20 13:57:29 -0300

    🎨 Corrigindo rotas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import {
   CADASTRAR_USUARIO,
   PAGINA_INICIAL,
   CADASTRAR_CURSO,
+  LISTAR_CURSOS,
   CADASTRAR_AVALIACAO,
-  CADASTRAR_DISCIPLINA,
-  CRIAR_MONITORIA,
+  // CADASTRAR_DISCIPLINA,
+  LISTAR_DISCIPLINAS,
+  CADASTRAR_MONITORIA,
   EDITAR_MONITORIA,
   MINHAS_MONITORIAS,
   LOGIN,
-} from "./routes";
+} from './routes';
 
-import { PaginaInicial } from "./pages/PaginaInicial";
-import { CadastrarUsuario } from "./pages/Usuario/Cadastrar";
-import { CadastrarCurso } from "./pages/Curso/Cadastrar";
-import { Avaliacao } from "./pages/Avaliacao/Cadastrar";
-import { CadastrarDisciplina } from "./pages/Cadastro/Disciplina";
-import { CriarMonitoria } from "./pages/Cadastro/Monitoria/CriarMonitoria";
-import { EditarMonitoria } from "./pages/Cadastro/Monitoria/EditarMonitoria";
-import { MinhasMonitorias } from "./pages/Cadastro/Monitoria/MinhasMonitorias";
-import { Login } from "./pages/Login";
+import {
+  PaginaInicial,
+  CadastrarUsuario,
+  Login,
+  CadastrarCurso,
+  Avaliacao,
+  // CadastrarDisciplina,
+  ListarDisciplinas,
+  CadastrarMonitoria,
+  EditarMonitoria,
+  MinhasMonitorias,
+  ListarCursos,
+} from './pages';
 
 export const App = () => {
   return (
@@ -28,16 +34,27 @@ export const App = () => {
         <Route exact path={PAGINA_INICIAL} element={<PaginaInicial />} />
         <Route exact path={CADASTRAR_USUARIO} element={<CadastrarUsuario />} />
         <Route exact path={CADASTRAR_CURSO} element={<CadastrarCurso />} />
+        <Route exact path={LISTAR_CURSOS} element={<ListarCursos />} />
         <Route exact path={CADASTRAR_AVALIACAO} element={<Avaliacao />} />
-        <Route
+        {/* <Route
           exact
           path={CADASTRAR_DISCIPLINA}
           element={<CadastrarDisciplina />}
+        /> */}
+        <Route
+          exact
+          path={LISTAR_DISCIPLINAS}
+          element={<ListarDisciplinas />}
         />
-        <Route exact path={CRIAR_MONITORIA} element={<CriarMonitoria />} />
+        <Route
+          exact
+          path={CADASTRAR_MONITORIA}
+          element={<CadastrarMonitoria />}
+        />
         <Route exact path={EDITAR_MONITORIA} element={<EditarMonitoria />} />
         <Route exact path={MINHAS_MONITORIAS} element={<MinhasMonitorias />} />
         <Route exact path={LOGIN} element={<Login />} />
+        <Route path="*" element={<Navigate to={LOGIN} />} />
       </Routes>
     </BrowserRouter>
   );

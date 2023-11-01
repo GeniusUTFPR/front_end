d9abe994ca66ce91b6135c200e57eecf8f4bda63
RafaelaTieri <rafaelaiwamoto302@gmail.com>
2023-11-01 00:18:47 -0300

    :sparkles: Listar paluno

--- src/pages/Professor/Paluno/Listar/index.js
+++ src/pages/Professor/Paluno/Listar/index.js
@@ -1,1 +1,31 @@
-export const ListarPaluno = () => {};
+import "./style.css";
+
+import { PalunoForm } from "../../../../components";
+import { Link } from "react-router-dom";
+
+import Header from "../../../../components/Header";
+import Footer from "../../../../components/Footer";
+
+export const ListarPaluno = () => {
+  return (
+    <div>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="listar-meu-paluno-container">
+        <div className="listar-meu-paluno-titulo">
+          Meu <span className="monitoria">&nbsp;PAluno</span>
+        </div>
+        <PalunoForm />
+        <div className="listar-meu-paluno-button">
+          <Link to="/paluno/cadastrar">
+            <button>Cadastrar PAluno</button>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
